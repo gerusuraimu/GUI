1f36c252664f07bdbb4743f42032bcf35eda6f8f
gerusuraimu <tohkams001@gmail.com>
2023-11-22 20:34:10 +0900

    test

---
+++ Func/Data.py
@@ -0,0 +1,11 @@
+class Data:
+    __title = 'window'
+    __geometry = '1920x1080'
+
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def geometry(self):
+        return self.__geometry
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import tkinter as tk
 import cv2 as cv
-from Func import FrameGetter
+from Func import Data, FrameGetter
 
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
+
+        self.data = Data.Data()
+        self.ui_init()
+
+    def ui_init(self):
+        self.title(self.data.title)
+        self.geometry(self.data.geometry)
 
 
 def main():
